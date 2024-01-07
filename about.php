2b7778b4b1f064b9849d572f67aea522a0b442ae
Micah <micah.botha@gmail.com>
2024-01-07 15:07:20 +0100

    changed file structure

---
+++ about.php
@@ -0,0 +1,268 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap"
+    rel="stylesheet">
+
+  <title>Bytebazaar - About Page</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+  <!-- Additional CSS Files -->
+  <link rel="stylesheet" href="assets/css/fontawesome.css">
+  <link rel="stylesheet" href="assets/css/templatemo-tale-seo-agency.css">
+  <link rel="stylesheet" href="assets/css/owl.css">
+  <link rel="stylesheet" href="assets/css/animate.css">
+  <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+  <!--
+
+TemplateMo 582 Tale SEO Agency
+
+https://templatemo.com/tm-582-tale-seo-agency
+
+-->
+</head>
+
+<body>
+
+  <!-- ***** Preloader Start ***** -->
+  <div id="js-preloader" class="js-preloader">
+    <div class="preloader-inner">
+      <span class="dot"></span>
+      <div class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  </div>
+  <!-- ***** Preloader End ***** -->
+
+  <!-- ***** Header Area Start ***** -->
+  <header class="header-area header-sticky">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <nav class="main-nav">
+            <!-- ***** Logo Start ***** -->
+            <a href="index.html" class="logo">
+              <img src="assets/images/logo.png" alt="" style="max-width: 300px; margin: -10px;">
+            </a>
+            <!-- ***** Logo End ***** -->
+            <!-- ***** Menu Start ***** -->
+            <ul class="nav">
+              <li><a href="index.html">Home</a></li>
+              <li><a href="index.html">Services</a></li>
+              <li><a href="index.html">Bestsellers</a></li>
+              <li class="has-sub">
+                <a href="javascript:void(0)">Pages</a>
+                <ul class="sub-menu">
+                  <li><a href="about.html">About Us</a></li>
+                  <li><a href="faqs.html">FAQs</a></li>
+                </ul>
+              </li>
+              <li><a href="index.html">Contact</a></li>
+            </ul>
+            <a class="menu-trigger">
+              <span>Menu</span>
+            </a>
+            <!-- ***** Menu End ***** -->
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
+  <!-- ***** Header Area End ***** -->
+
+  <div class="page-heading">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-7 align-self-center">
+          <div class="caption  header-text">
+            <h6>Bytebazaar</h6>
+            <div class="line-dec"></div>
+            <h4>Discover More <em>About Us</em></h4>
+            <p>
+              Welcome to ByteBazaar, your go-to destination for affordable CD keys. Founded with a mission to simplify access for developers and make gaming budget-friendly for players.</p>
+            <div class="main-button"><a href="faqs.html">Check our FAQs</a></div>
+          </div>
+        </div>
+        <div class="col-lg-5 align-self-center">
+          <img class="rounded-5" src="assets/images/about-us-image-edited.jpg" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="video-info section">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="video-thumb">
+            <img src="assets/images/controller.jpg" alt="">
+          </div>
+        </div>
+        <div class="col-lg-6 align-self-center">
+          <div class="section-heading">
+            <h2>Detailed Information On What We Do &amp; Who We Are</h2>
+            <div class="line-dec"></div>
+            <p>Welcome to ByteBazaar, where we are redefining the CD key experience for developers and gamers alike.<br><br>
+
+              <b>For Developers:</b><br>
+              ByteBazaar is your secure marketplace, 
+              eliminating grey market sellers. 
+              We provide a dedicated space for developers to distribute CD keys, 
+              ensuring fair compensation and protecting the gaming industry's integrity. <br><br>
+
+              <b>For Gamers:</b><br>
+              Enjoy a trustworthy gaming experience at ByteBazaar. 
+              Every CD key is legitimate, sourced directly from developers or authorized distributors. 
+              ByteBazaar is your key to a safer and more enjoyable gaming journey. <br><br>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="happy-clients section">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="section-heading">
+            <h2>Our 4 Principles For Happy Customers</h2>
+            <div class="line-dec"></div>
+          </div>
+        </div>
+        <div class="col-lg-12">
+          <div class="naccs">
+            <div class="tabs">
+              <div class="row">
+                <div class="col-lg-12">
+                  <div class="menu">
+                    <div class="active"><span>Affordable Prices</span></div>
+                    <div><span>Authentic Sources</span></div>
+                    <div><span>Reliability</span></div>
+                    <div class="last-item"><span>Customer Security</span></div>
+                  </div>
+                </div>
+                <div class="col-lg-12">
+                  <ul class="nacc">
+                    <li class="active">
+                      <div>
+                        <div class="row">
+                          <div class="col-lg-7">
+                            <h4>Why is it so important?</h4>
+                            <div class="line-dec"></div>
+                            <p>At ByteBazaar, our core principle is to make cutting-edge technology and gaming experiences accessible to all. 
+                              We believe affordability is not just a feature but a fundamental right. By consistently offering competitive and budget-friendly prices, 
+                              we ensure that innovation and enjoyment are within reach for every user. 
+                              ByteBazaar stands as a beacon, advocating for inclusivity in the digital realm, 
+                              where cost should never be a barrier to exploring the limitless possibilities of technology and gaming.</p>
+                          </div>
+                          <div class="col-lg-5 align-self-center">
+                            <img src="assets/images/happyclient-01.jpg" alt="">
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                    <li>
+                      <div>
+                        <div class="row">
+                          <div class="col-lg-7">
+                            <h4>Information about the Source</h4>
+                            <div class="line-dec"></div>
+                            <p>At ByteBazaar, authenticity is our guiding principle. 
+                              We are committed to sourcing CD keys directly from developers and authorized distributors, 
+                              ensuring a transparent and genuine marketplace. By adhering to this principle, 
+                              we guarantee that every product on our platform is legitimate and untainted by unauthorized resellers. 
+                              ByteBazaar stands as a bastion against the uncertainties of the grey market, providing users with the confidence that their CD keys are authentic, 
+                              secure, and ethically acquired. Your trust in our commitment to authenticity is the foundation of our platform.</p>
+                          </div>
+                          <div class="col-lg-5 align-self-center">
+                            <img src="assets/images/happyclient-01.jpg" alt="">
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                    <li>
+                      <div>
+                        <div class="row">
+                          <div class="col-lg-7">
+                            <h4>Reliability</h4>
+                            <div class="line-dec"></div>
+                            <p>Reliability is the cornerstone of ByteBazaar. We prioritize providing a dependable platform for developers and gamers alike. 
+                              Our commitment to reliability encompasses seamless transactions, accurate information, and consistent support. 
+                              ByteBazaar stands as a reliable partner, ensuring that every CD key transaction is smooth, every product is as described, 
+                              and every user can trust our platform for a trustworthy and consistent experience. 
+                              Your satisfaction and confidence in the reliability of ByteBazaar are paramount to our mission.</p>
+                          </div>
+                          <div class="col-lg-5 align-self-center">
+                            <img src="assets/images/happyclient-01.jpg" alt="">
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                    <li>
+                      <div>
+                        <div class="row">
+                          <div class="col-lg-7">
+                            <h4>Information about our security</h4>
+                            <div class="line-dec"></div>
+                            <p>At ByteBazaar, customer security is our top priority. 
+                              We employ rigorous measures to safeguard your personal information and ensure secure transactions. 
+                              Our commitment to customer security involves using the latest encryption technologies, adhering to industry best practices, 
+                              and continuously evolving our security protocols. ByteBazaar is dedicated to providing you with a safe and secure environment, 
+                              where you can confidently explore, purchase, and enjoy the world of CD keys. 
+                              Your trust in the security of your transactions is at the heart of our commitment to excellence.</p>
+                          </div>
+                          <div class="col-lg-5 align-self-center">
+                            <img src="assets/images/happyclient-01.jpg" alt="">
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<footer>
+    <div class="container">
+      <div class="col-lg-12">
+        <p>Copyright © 2023 <a href="#">ByteBazaar</a>. All rights reserved.
+
+          <br>Design: <a href="https://templatemo.com" target="_blank">TemplateMo</a> Distribution: <a
+            href="https://themewagon.com">ThemeWagon</a>
+        </p>
+      </div>
+    </div>
+  </footer>
+
+
+  <!-- Scripts -->
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+  <script src="assets/js/isotope.min.js"></script>
+  <script src="assets/js/owl-carousel.js"></script>
+  <script src="assets/js/tabs.js"></script>
+  <script src="assets/js/popup.js"></script>
+  <script src="assets/js/custom.js"></script>
+
+</body>
+
+</html>
