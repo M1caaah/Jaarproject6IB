209082edf2f0cfa41388cd501d55bf03466dd0d2
m1caaah <micah.botha@gmail.com>
2024-01-10 08:44:47 +0100

    cleaned up icons and added github link

--- dashboard/index.php
+++ dashboard/index.php
@@ -87,42 +87,10 @@
 
       <!-- Right links -->
       <ul class="navbar-nav ms-auto d-flex flex-row">
-        <!-- Notification dropdown -->
-        <li class="nav-item dropdown">
-          <a
-             class="nav-link me-3 me-lg-0 dropdown-toggle hidden-arrow"
-             href="#"
-             id="navbarDropdownMenuLink"
-             role="button"
-             data-mdb-toggle="dropdown"
-             aria-expanded="false"
-             >
-            <i class="fas fa-bell"></i>
-            <span class="badge rounded-pill badge-notification bg-danger"
-                  >1</span
-              >
-          </a>
-          <ul
-              class="dropdown-menu dropdown-menu-end"
-              aria-labelledby="navbarDropdownMenuLink"
-              >
-            <li><a class="dropdown-item" href="#">Some news</a></li>
-            <li><a class="dropdown-item" href="#">Another news</a></li>
-            <li>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </li>
-          </ul>
-        </li>
 
         <!-- Icon -->
-        <li class="nav-item">
-          <a class="nav-link me-3 me-lg-0" href="#">
-            <i class="fas fa-fill-drip"></i>
-          </a>
-        </li>
-        <!-- Icon -->
         <li class="nav-item me-3 me-lg-0">
-          <a class="nav-link" href="#">
+          <a class="nav-link" href="https://github.com/M1caaah/Jaarproject6IB">
             <i class="fab fa-github"></i>
           </a>
         </li>
