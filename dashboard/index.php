27a386149f7824e43eb8d6760194e7c23c1843e8
m1caaah <micah.botha@gmail.com>
2024-01-10 08:28:16 +0100

    Added link to local CSS and moved .php files

--- dashboard/index.php
+++ dashboard/index.php
@@ -11,7 +11,7 @@
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet"/>
   <!-- MDB -->
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.1.0/mdb.min.css" rel="stylesheet"/>
+  <link rel="stylesheet" href="css/mdb.min.css" />
   <!-- Custom stylesheet -->
   <link rel="stylesheet" href="../assets/css/style.css"></head>
 
