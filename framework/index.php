2a5237f46a1267d30e4cec83310b2d5fb5170b2d
Micah <micah.botha@gmail.com>
2024-01-31 14:12:23 +0100

    Made framework folder and started implementing routing

---
+++ framework/index.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once __DIR__.'/vendor/autoload.php';
+
+$app = new \app\core\Application();
+
+$app->router->get('/', function(){
+     return 'Hello world';
+});
+
+$app->router->get('/contact', function(){
+    return 'Contact';
+});
+
+$app->run();
