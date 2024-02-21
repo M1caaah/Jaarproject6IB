2ee1b0bc688785b65bd3ee103ab149aba73b3012
Micah <micah.botha@gmail.com>
2024-02-21 14:21:46 +0100

    Trying to add sidebar

--- framework/views/layouts/dashboard.php
+++ framework/views/layouts/dashboard.php
@@ -11,12 +11,70 @@
 </head>
 
 <body>
+<!--Main Navigation-->
+<header>
+    <!-- Sidebar -->
+    <nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse bg-dark">
+        <div class="position-sticky">
+            <div class="list-group list-group-flush mx-3 mt-4">
+                <a href="index.php" class="list-group-item list-group-item-action rounded-9 py-2 ripple active" aria-current="true">
+                    <i class="fas fa-user-gear fa-fw me-3"></i>
+                    <span>Users</span>
+                </a>
+                <a href="coming-soon.php" class="list-group-item list-group-item-action rounded-9 py-2 ripple">
+                    <i class="far fa-circle-question fa-fw me-3"></i>
+                    <span>Coming soon...</span>
+                </a>
+            </div>
+        </div>
+    </nav>
+    <!-- Sidebar -->
 
+    <!-- Navbar -->
+    <nav id="main-navbar" class="navbar navbar-expand-lg navbar-light bg-white fixed-top">
+        <!-- Container wrapper -->
+        <div class="container-fluid">
+            <!-- Toggle button -->
+            <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fas fa-bars"></i>
+            </button>
 
+            <!-- Brand -->
+            <a class="navbar-brand" href="index.php">
+                <img src="assets/img/logos/logo.png" height="35" alt="" loading="lazy" />
+            </a>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min.js"></script>
-<script src="assets/js/script.min.js"></script>
+            <!-- Right links -->
+            <ul class="navbar-nav ms-auto d-flex flex-row">
+
+                <!-- Icon -->
+                <li class="nav-item me-3 me-lg-0">
+                    <a class="nav-link" href="https://github.com/M1caaah/Jaarproject6IB">
+                        <i class="fab fa-github"></i>
+                    </a>
+                </li>
+
+                <!-- Avatar -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle hidden-arrow d-flex align-items-center" href="#" id="navbarDropdownMenuLink" role="button" data-mdb-toggle="dropdown" aria-expanded="false">
+                        <img src="https://mdbootstrap.com/img/Photos/Avatars/img (31).jpg" class="rounded-circle" height="22" alt="" loading="lazy" />
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
+                        <li><a class="dropdown-item" href="#">My profile</a></li>
+                        <li><a class="dropdown-item" href="#">Settings</a></li>
+                        <li><a class="dropdown-item" href="#">Logout</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <!-- Container wrapper -->
+    </nav>
+    <!-- Navbar -->
+</header>
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min.js"></script>
+	<script src="assets/js/script.min.js"></script>
 </body>
 
 </html>
