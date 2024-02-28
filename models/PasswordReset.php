ad1ba56a410025551beee84f0bcb16e1e1bb660f
Micah <micah.botha@gmail.com>
2024-02-28 17:08:48 +0100

    Password reset works

--- models/PasswordReset.php
+++ models/PasswordReset.php
@@ -2,6 +2,7 @@
 
 namespace app\models;
 
+use app\core\Application;
 use app\core\DbModel;
 
 class PasswordReset extends DbModel
@@ -44,13 +45,19 @@ class PasswordReset extends DbModel
     {
         return [
             'password' => [self::RULE_REQUIRED],
-            'newPassword' => [self::RULE_REQUIRED, [self::RULE_MIN, 'min' => 8]],
+            'newPassword' => [self::RULE_REQUIRED, [self::RULE_MIN, 'min' => 8], [self::RULE_MAX, 'max' => 255]],
             'confirmPassword' => [self::RULE_REQUIRED, [self::RULE_MATCH, 'match' => 'newPassword']]
         ];
     }
 
-    public function resetPassword()
+    public function updatePassword()
     {
-
+        if (!password_verify($this->password, Application::$app->user->password)) {
+            $this->addError('password', 'Password is incorrect');
+            return false;
+        }
+        $this->password = password_hash($this->newPassword, PASSWORD_DEFAULT);
+        $this->update(Application::$app->session->get('user'));
+        return true;
     }
 }
