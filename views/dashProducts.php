35ff18415ca747807cc923b47a16cff975ccf5b4
Micah <micah.botha@gmail.com>
2024-04-13 17:30:16 +0200

    Products can now be viewed in the manage products tab

--- views/dashProducts.php
+++ views/dashProducts.php
@@ -11,31 +11,31 @@ use app\models\DashAddUsers;
             <h4>Manage products</h4>
         </div>
         <div class="row">
+            <?php foreach ($model->select(['*']) as $product): ?>
                 <div class="col-md-6 col-lg-4 col-xl-3">
                     <div class="card border-0">
                         <div class="card-body">
+                            <img src="<?= $product['imagePath'] ?>" alt="" class="img-fluid">
                             <h5 class="card-title"></h5>
                             <div class="row">
                                 <div class="col-12">
-                                    <p class="card-text text-muted">Email: </p>
+                                    <p class="card-text text-muted">Product name: <?= $product['productName'] ?></p>
                                 </div>
                                 <div class="col-12">
-                                    <p class="card-text text-muted">Role:  </p>
-                                </div>
-                                <div class="col-12">
-                                    <p class="card-text text-muted">Registration date:  </p>
+                                    <p class="card-text text-muted">Price: &euro;<?= $product['price'] ?></p>
                                 </div>
                             </div>
                             <div class="dropdown mt-3">
                                 <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">Menu</button>
                                 <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                    <li><a class="dropdown-item" href="users/edit?client_id=">Edit User</a></li>
-                                    <li><a class="dropdown-item" href="users/delete?client_id=">Delete User</a></li>
+                                    <li><a class="dropdown-item" href="users/edit?product_id=<?= $product['product_id'] ?>">Edit User</a></li>
+                                    <li><a class="dropdown-item" href="users/delete?product_id=<?= $product['product_id'] ?>">Delete User</a></li>
                                 </ul>
                             </div>
                         </div>
                     </div>
                 </div>
+            <?php endforeach; ?>
         </div>
     </div>
 </main>
