7e544554a4bd081ad6f4f639646b40f932862164
Micah <micah.botha@gmail.com>
2024-05-02 18:26:49 +0200

    Fixed small oversight with OrderController using DashUsers model

--- controllers/DashOrderController.php
+++ controllers/DashOrderController.php
@@ -3,13 +3,13 @@
 namespace app\controllers;
 
 use app\core\Controller;
-use app\models\DashUsers;
+use app\models\DashOrders;
 
 class DashOrderController extends Controller
 {
     public function orders(): array|bool|string
     {
-        $dashOrders = new DashUsers();
+        $dashOrders = new DashOrders();
         return $this->render('dashOrders', 'dashboard', ['model' => $dashOrders]);
     }
 }
