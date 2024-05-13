9f66d3fde307a482334e2e37304e07331e1aaefa
M1caaah <micah.botha@gmail.com>
2024-05-13 09:21:29 +0200

    show display_errors

--- public_html/index.php
+++ public_html/index.php
@@ -9,6 +9,8 @@ use app\controllers\SiteController;
 use app\controllers\UserController;
 use app\core\Application;
 
+ini_set('display_errors', 1);
+
 require_once __DIR__ . '/../vendor/autoload.php';
 $dotenv = \Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
