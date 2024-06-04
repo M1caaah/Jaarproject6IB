5a93a590fcc2c562cf437d3184c4ef75c87adcc6
M1caaah <73669490+M1caaah@users.noreply.github.com>
2024-06-04 20:13:58 +0200

    feature: Activate and deactivate items
    * Show inactive users
    
    * Reworked some middlewares to block all routes by default
    
    * Added necessary routes
    
    * Added button to switch between active and inactive views
    
    * Added active checks for products
    
    * Added active checks for orders
    
    * Fixed checkActive clause in select function
    
    * Removed checkActive: false in getOrders
    
    * Fixed findAllOrders to no longer check for active unless asked to
    
    * Added delete and activate buttons to orders
    
    * Fixed active checks for the select functions
    
    * Fixed activation links for products and users
    
    * Added check to make sure no inactive user orders are being shown
    
    * Removed unnecessary actions in registerMiddleware
    
    * Orders can now be deleted
    
    * Added activate function
    
    * Added activate function
    
    * User can now be reactivated
    
    * Product activation now works

--- controllers/DashOrderController.php
+++ controllers/DashOrderController.php
@@ -3,6 +3,8 @@
 namespace app\controllers;
 
 use app\core\Controller;
+use app\core\Request;
+use app\core\Response;
 use app\middlewares\DashMiddleware;
 use app\models\DashOrders;
 
@@ -14,14 +16,31 @@ class DashOrderController extends Controller
 {
     function __construct()
     {
-        $this->registerMiddleware(new DashMiddleware(
-            ['orders']
-        ));
+        $this->registerMiddleware(new DashMiddleware());
     }
 
-    public function orders(): array|bool|string
+    public function orders(Request $request, Response $response): array|bool|string
     {
         $dashOrders = new DashOrders();
+        $dashOrders->loadData($request->getBody());
         return $this->render('dashOrders', 'dashboard', ['model' => $dashOrders]);
+    }
+
+    public function deleteOrder(Request $request, Response $response): bool
+    {
+        $dashOrders = new DashOrders();
+        $dashOrders->loadData($request->getBody());
+        $dashOrders->deactivate($dashOrders->order_id, 'tblorders', 'order_id');
+        $response->redirect('/dashboard/orders');
+        return true;
+    }
+
+    public function activateOrder(Request $request, Response $response): bool
+    {
+        $dashOrders = new DashOrders();
+        $dashOrders->loadData($request->getBody());
+        $dashOrders->activate($dashOrders->order_id, 'tblorders', 'order_id');
+        $response->redirect('/dashboard/orders');
+        return true;
     }
 }
